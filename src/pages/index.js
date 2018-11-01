b7e568871cde45b1d1aa44e106c82f7e2f9208ae
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-31 21:59:03 -0400

    add back image

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/project-one/">Go to project 1</Link>
+    <Link to="/project-one/">Go to page 2</Link>
+    <Link to="/project-one/">Go to project 3</Link>
   </Layout>
 )
 
