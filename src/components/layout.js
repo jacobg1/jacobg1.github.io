63e1e46411c21afdf48df844e59caa34b1d18ede
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-02 23:40:56 -0400

    update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
@@ -19,7 +21,7 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <> 
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
@@ -32,7 +34,7 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: '0 auto',
+            margin: '225px auto 0 auto',
             maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
