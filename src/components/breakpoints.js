4417cc09a56b37796be0f5a71c11810438f287b3
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-07 19:25:05 -0500

    add extra breakpoint

--- src/components/breakpoints.js
+++ src/components/breakpoints.js
@@ -1,6 +1,7 @@
 
 export const breakpoints = {
     mobile: `(min-width: 425px)`,
+    footer: `(min-width: 500px)`,
     tablet: `(min-width: 768px)`,
     laptop: `(min-width: 1024px)`,
     desktop: `(min-width: 1440px)`,
