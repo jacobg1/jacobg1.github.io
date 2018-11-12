7b9891ed85e41c8b5ec695947dbf89677ddbdaa7
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-12 15:34:21 -0500

    adding a breakpoint in between mobile and tablet

--- src/components/breakpoints.js
+++ src/components/breakpoints.js
@@ -3,6 +3,7 @@ export const breakpoints = {
     header: `(min-width: 325px)`,
     mobile: `(min-width: 425px)`,
     footer: `(min-width: 500px)`,
+    specialHeader: `(min-width: 657px)`,
     tablet: `(min-width: 768px)`,
     laptop: `(min-width: 1024px)`,
     desktop: `(min-width: 1440px)`,
