974285d848d74053a3a8dac0c47596a6d4c11a2c
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-12 18:23:30 -0500

    adding contact form page and component

---
+++ src/components/contactForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import { breakpoints } from './breakpoints'
+
+
+class ContactForm extends Component {
+    render () {
+        return (
+            <React.Fragment>
+                <h1>contact form</h1>
+            </React.Fragment>
+        )
+    }
+    
+}
+
+export default ContactForm
---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import ContactForm from '../components/contactForm'
+// import styled from 'styled-components'
+// import { breakpoints } from '../components/breakpoints';
+
+
+const ContactPage = () => (
+    <Layout>
+
+        <ContactForm />
+    </Layout>
+)
+
+export default ContactPage
