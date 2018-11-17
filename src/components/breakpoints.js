7e785563f4c3f3b70cf681b54ddcf5d20a4c425e
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-17 17:57:01 -0500

    tweaking numbers

--- src/components/breakpoints.js
+++ src/components/breakpoints.js
@@ -2,7 +2,7 @@
 export const breakpoints = {
     header: `(min-width: 325px)`,
     mobile: `(min-width: 425px)`,
-    footer: `(min-width: 500px)`,
+    footer: `(min-width: 560px)`,
     specialHeader: `(min-width: 657px)`,
     tablet: `(min-width: 768px)`,
     laptop: `(min-width: 1024px)`,
