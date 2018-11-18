b6f2ce04ec814857bbf7d9b3a146911de173aec3
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-18 16:13:37 -0500

    styling 404 page a bit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import Layout from '../components/layout'
+import styled from 'styled-components'
+import { breakpoints } from '../components/breakpoints'
+import { Link } from 'gatsby'
+
+const ContentCard = styled.div`
+  padding: 15px;
+  width: 91%;
+  margin: 125px auto 104px auto;
+  background: #f1f1f1;
+  border-radius: 5px;
+  max-width: 400px;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+
+  @media ${breakpoints.tablet} {            
+    max-width: 613px;
+    padding: 25px;
+    text-align: center;
+  }
+
+  @media ${breakpoints.laptop} {
+    max-width: 700px;
+  }
+
+  & h2, p {
+    color: #0c1d96;
+  }
+  & a {
+    color: #ef6061;
+    text-decoration: none;
+    transition: color .3s ease-in-out;
+    :hover {
+      color: #0c1d96;
+    }
+  }
+`
+
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <ContentCard>
+      <h2>Page not found</h2>
+      <p>You just hit a route that doesn&#39;t exist... </p>
+      <p>Back to <Link to="/">homepage</Link></p>
+    </ContentCard>
   </Layout>
 )
 
