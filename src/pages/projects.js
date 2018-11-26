cd8e206db093f48536d50566020415333ab1ae9a
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-26 11:54:48 -0500

    adding projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+// import styled from 'styled-components'
+// import { breakpoints } from '../components/breakpoints';
+import ProjectsComponent from '../components/projectsComponent'
+
+const ProjectsPage = () => (
+   
+    <Layout>
+        <ProjectsComponent />
+    </Layout>
+
+)
+
+export default ProjectsPage
