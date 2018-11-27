8d3beb1a70940d44490cde99c23ff47af83c9f4b
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-26 20:27:26 -0500

    change special header breakpoint size

--- src/components/breakpoints.js
+++ src/components/breakpoints.js
@@ -3,7 +3,7 @@ export const breakpoints = {
     header: `(min-width: 325px)`,
     mobile: `(min-width: 425px)`,
     footer: `(min-width: 560px)`,
-    specialHeader: `(min-width: 657px)`,
+    specialHeader: `(min-width: 700px)`,
     tablet: `(min-width: 768px)`,
     laptop: `(min-width: 1024px)`,
     desktop: `(min-width: 1440px)`,
