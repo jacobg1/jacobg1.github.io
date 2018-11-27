895e8867390554dc2c3bf2873a12d85bcca84b99
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-26 20:25:26 -0500

    add page to show resume

---
+++ src/pages/resume.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import MyPdfViewer from '../components/myPdf'
+
+
+const ResumePage = () => (
+
+    <Layout>
+        <MyPdfViewer />
+    </Layout>
+
+)
+
+export default ResumePage
