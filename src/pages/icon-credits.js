6be3ed4435f562cc6366f3afd5fe2bdfe610a12d
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-26 20:54:49 -0500

    update with file download icon

--- src/pages/icon-credits.js
+++ src/pages/icon-credits.js
@@ -47,7 +47,7 @@ const IconCredit = () => (
       <p>Record player icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></p>
       <p>Simon icon made by <a href="https://www.flaticon.com/authors/creaticca-creative-agency" title="Creaticca Creative Agency">Creaticca Creative Agency</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></p>
       <p>Favicon provided by <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></p>
-      <p>Github and rocket icons provided by <a href="https://fontawesome.com/license" terget="_blank" rel="noopener noreferrer">Font Awesome</a></p>
+      <p>Github, rocket and file download icons provided by <a href="https://fontawesome.com/license" terget="_blank" rel="noopener noreferrer">Font Awesome</a></p>
       <Link to="/">Go back to the homepage</Link>
     </ContentCard>
   </Layout>
