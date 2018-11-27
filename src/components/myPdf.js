bb516e1b89d0e717f39dd4c7b8032a9143005ab6
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-26 20:55:15 -0500

    add download icon

--- src/components/myPdf.js
+++ src/components/myPdf.js
@@ -4,10 +4,15 @@ import PDF from 'react-pdf-js-infinite';
 import styled from 'styled-components'
 import resume from '../images/resume.pdf'
 import { breakpoints } from '../components/breakpoints'
+import downloadIcon from '../images/file-download-solid.svg'
 
 const ResumeHolder = styled.div`
     text-align: center;
-    padding-top: 50px;
+    padding-top: 45px;
+
+    @media ${breakpoints.laptop} {
+        padding-top: 75px;
+    }
 
     & canvas {
         max-width: 300px;
@@ -21,6 +26,21 @@ const ResumeHolder = styled.div`
             max-width: 850px;
         }
     }
+
+    & img {
+        width: 35px;
+        
+        @media ${breakpoints.laptop} {
+            width: 50px;
+            position: absolute;
+            right: 16px;
+        }
+
+        @media ${breakpoints.desktop} {
+            right: 95px;
+            width: 75px;
+        }
+    }
 `
 
 export default class MyPdfViewer extends React.Component {
@@ -28,6 +48,7 @@ export default class MyPdfViewer extends React.Component {
     render() {
         return (
             <ResumeHolder>
+                <a href={ resume } download><img src={downloadIcon} /></a>
                 <PDF file={resume} scale={2} />
             </ResumeHolder>
         )
